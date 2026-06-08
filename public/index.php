d374e9c84c9773c2a94bdde81fd644d0b1171206
moussaguillaume <moussaguillaume@hotmail.fr>
2026-06-08 12:31:06 +0200

    "Setup application configuration and environment variables"

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
-header('Content-Type: application/json; charset=utf-8');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+header('Content-Type: application/json');
 
 echo json_encode([
     'project' => 'MySmartAgenda BackEnd',
-    'status' => 'running'
+    'status' => 'running',
+    'environment' => $_ENV['APP_ENV']
 ]);
